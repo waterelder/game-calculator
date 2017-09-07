1e2dafe43d3c1270f644138400034936509aa8e2
Alex Kalina <kalinalex91@gmail.com>
2017-09-07 10:20:33 +0300

    Fix readme №2

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const Pet = require("./pet");
 const config = require("./config");
 const myPet = new Pet(config.stats, 0, 0, 0, 1000);
 
+// ПИСАТЬ ТУТ
+
 myPet.switchTime(100);
 myPet.applyInfluence(config.inflenses[0]);
+
+
+//////////////////////
+
+process.exit(0);
